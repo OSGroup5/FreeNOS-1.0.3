fba63d06d86a9e0e649b01e7b04fa39e28af25ef
jessicamargala <56134693+jessicamargala@users.noreply.github.com>
2021-10-26 19:23:51 -0700

    Adds kill functionality
    Kill command to kill a background process given the PID

---
+++ bin/kill/Kill.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <Process.h>
+#include <ProcessClient.h>
+#include "Kill.h"
+
+
+Kill::Kill(int argc, char** argv) : POSIXApplication(argc, argv)
+{
+	parser().setDescription("Kill a background process.");
+	parser().registerPositional("PID", "The process id of a background process to kill.");
+}
+
+Kill::~Kill() 
+{
+	
+}
+
+Kill::Result Kill::exec()
+{
+    // Get Process ID of a currently running process
+    ProcessID pid = atoi(arguments().get("PID"));
+
+
+    //ProcessClient client;
+    //ProcessClient::Info info;
+    //const ProcessClient::Result result = process.processInfo(pid, info);
+
+    //const API::Result result = ProcessCtl(pid, KillPID, (Address) &info.kernelState);
+    //const API::Result result = ProcessCtl(pid, KillPID);
+
+    ProcessCtl(pid, KillPID);
+
+    // Done
+    return Success;
+}
